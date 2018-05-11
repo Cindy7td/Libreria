31e2322f9984cabae4bf80f6bb693599e8c145e1
Cindy7td <chocolatectd@gmail.com>
2018-05-11 17:57:40 -0600

    muchos cambios

---
+++ LibreriaBasuraForms/LibreriaBasuraForms/Classes/UVendedor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaBasuraForms.Classes
+{
+    class UVendedor : Usuario
+    {
+        List<Libro> misLibros;
+
+        public UVendedor(string nombre, float dinero): base(nombre, dinero)
+        {
+            misLibros = new List<Libro>();
+        }
+
+        internal List<Libro> MisLibros { get => misLibros; set => misLibros = value; }
+
+
+    }
+}
---
+++ LibreriaBasuraForms/LibreriaBasuraForms/Utilities/IO/Lectura.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaBasuraForms.Utilities.IO
+{
+    class Lectura
+    {
+        public static string[] leer()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Temp\Log_in.txt");
+
+            return lines;
+        }
+    }
+}
